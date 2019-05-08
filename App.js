5e13c2edb13c6aaa39509c9afc5e103c21543852
nhut_pham <nhut.pham@ilogic.vn>
2019-05-08 17:56:33 +0700

    config react-native-vector-icons

--- App.js
+++ App.js
@@ -9,21 +9,17 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Icon from "react-native-vector-icons/Ionicons";
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Vector icons</Text>
+        <Icon name="md-home" size={25} />
+        <Icon name="md-book" size={25} />
+        <Icon name="md-settings" size={25} />
       </View>
     );
   }
